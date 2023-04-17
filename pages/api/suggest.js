c01ce15e5806776bd27f55dc1e8558a02fea6532
lareb.syed <lareb_khalil_syed@hotmail.com>
2023-04-17 11:40:54 +0500

    『:bug:』Fix: suggest

--- pages/api/suggest.js
+++ pages/api/suggest.js
@@ -3,11 +3,11 @@ const axios = require(axios);
 
 export default function handler(req, res) {
   const suggestions = axios.get(
-    "https://www.google.com/complete/search?client=firefox&q=res.query"
+    `https://www.google.com/complete/search?client=firefox&q=${req.query.question}`
   );
   const answers = axios.get(
-    "https://serpapi.com/search.json?engine=google&q=1+USD&location=Pakistan&google_domain=google.com.pk&gl=pk&hl=en&nfpr=1&api_key=e6e7ace950e132ff8350a014cf431130133c21cb293f4a7c2369b00d885246c6"
+    `https://serpapi.com/search.json?engine=google&q=${req.query.question}&location=Pakistan&google_domain=google.com.pk&gl=pk&hl=en&nfpr=1&api_key=e6e7ace950e132ff8350a014cf431130133c21cb293f4a7c2369b00d885246c6`
   );
-  if (answers.data) suggestion.data[1].push(anwer.data.result);
+  if (answers.data) suggestions.data[1].push(answers.data.result);
   res.status(200).json(suggestions.data);
 }
