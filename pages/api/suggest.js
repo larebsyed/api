1b62aa3d619052003bf5db7c2ace2dd28eb0c490
lareb.syed <lareb_khalil_syed@hotmail.com>
2023-04-17 11:49:59 +0500

    『:bug:』Fix: suggest

--- pages/api/suggest.js
+++ pages/api/suggest.js
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const axios = require("axios");
 
-export default function handler(req, res) {
-  const suggestions = axios.get(
+export default async function handler(req, res) {
+  console.log(req.query.question);
+  const suggestions = await axios.get(
     `https://www.google.com/complete/search?client=firefox&q=${req.query.question}`
   );
-  const answers = axios.get(
+
+  console.log(suggestions.data);
+
+  const answers = await axios.get(
     `https://serpapi.com/search.json?engine=google&q=${req.query.question}&location=Pakistan&google_domain=google.com.pk&gl=pk&hl=en&nfpr=1&api_key=e6e7ace950e132ff8350a014cf431130133c21cb293f4a7c2369b00d885246c6`
   );
-  if (answers.data) suggestions.data[1].push(answers.data.result);
+  console.log(answers.data);
+  if (answers.data?.answer_box?.result)
+    suggestions.data[1] = [
+      `${req.query.question} = ${answers?.data?.answer_box?.result}`,
+      ...suggestions.data[1],
+    ];
   res.status(200).json(suggestions.data);
 }
