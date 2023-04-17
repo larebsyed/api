33b5291ce1d069b5ca4d5b56397af7592780fdb2
lareb.syed <lareb_khalil_syed@hotmail.com>
2023-04-17 14:32:17 +0500

    『:heavy_plus_sign:』Add: currency

--- pages/api/currency.js
+++ pages/api/currency.js
@@ -8,22 +8,27 @@ export default async function handler(req, res) {
     if (!question) {
       res.status(200).send("");
     }
-    console.log(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${question
-        .replace(/to/g, "/")
-        .replace(/\s/g, "")
-        .replace(/\d*/, "")}.json`
-    );
+
+    const currencyPath = question
+      .replace(/to/g, "/")
+      .replace(/\s/g, "")
+      .replace(/\d*/, "");
+    console.log(currencyPath);
+
+    const currencies = currencyPath.match(/^(\w{3})\/(\w{3})$/);
+    console.log(currencies);
+    if (!currencies) {
+      res.status(200).send("");
+      return;
+    }
     const answers = await axios.get(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${question
-        .replace(/to/g, "/")
-        .replace(/\s/g, "")
-        .replace(/\d*/, "")}.json`
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencies[0]}.json`
     );
 
     res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate");
-    res.status(200).send(answers.data?.pkr);
+    res.status(200).send(answers.data?.[currencies[2]]);
   } catch (e) {
+    console.log(e);
     res.status(200).send("");
   }
 }
