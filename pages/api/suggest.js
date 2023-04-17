8a17e4cc6b969d69f0321587e248a2e540456fd0
lareb.syed <lareb_khalil_syed@hotmail.com>
2023-04-17 14:48:58 +0500

    『:bug:』Fix: issue

--- pages/api/suggest.js
+++ pages/api/suggest.js
@@ -6,27 +6,35 @@ export const config = {
 const plugins = ["currency", "calculate"];
 
 export default async function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const question = searchParams.get("question");
-  const suggestResponse = await fetch(
-    `https://www.google.com/complete/search?client=firefox&q=${question}`
-  );
-  const suggestions = await suggestResponse.json();
-
-  plugins.forEach(async (plugin) => {
-    const answerResponse = await fetch(
-      `${process.env.ANSWER_URI}/api/${plugin}?question=${question}`
+  try {
+    const { searchParams } = new URL(req.url);
+    const question = searchParams.get("question");
+    const suggestResponse = await fetch(
+      `https://www.google.com/complete/search?client=firefox&q=${question}`
     );
-    const answer = await answerResponse.text();
-    if (answer) {
-      suggestions[1] = [`${question} = ${answer}`, ...suggestions[1]];
-    }
-  });
+    const suggestions = await suggestResponse.json();
 
-  return new Response(JSON.stringify(suggestions), {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+    for (const plugin of plugins) {
+      const answerResponse = await fetch(
+        `${process.env.ANSWER_URI}/api/${plugin}?question=${question}`
+      );
+      const answer = await answerResponse.text();
+      console.log(" => ", answer);
+
+      if (answer) {
+        suggestions[1] = [`${question} = ${answer}`, ...suggestions[1]];
+      }
+    }
+
+    console.log(" => ", suggestions);
+
+    return new Response(JSON.stringify(suggestions), {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
